90a698abacee61c7d8bce683cf7a759b94fb863e
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-01-30 10:44:27 +0900

    Implemented static method to create instance for ERC20

--- packages/caver-klay/caver-klay-kct/erc20/src/index.js
+++ packages/caver-klay/caver-klay-kct/erc20/src/index.js
@@ -20,28 +20,58 @@ const _ = require('lodash')
 const Contract = require('../../../caver-klay-contract')
 const { ERC20_TYPES, ERC20_ABI_CODE } = require('../../kctHelper')
 const { isAddress } = require('../../../../caver-utils')
+const formatters = require('../../../../caver-core-helpers/src/formatters')
+const Method = require('../../../../caver-core-method')
+
+const CODE_HASH = {
+    SIMPLE: 'CDq9tSXfwfXczNr64+lGjyJIk+3Soj161pUdTiwEiZw=',
+    FULL: 'y8vE5WP/XH2zjic/RoBXTuD0B/MfDil6aNyHXeYbqLc=',
+}
 
 class ERC20 extends Contract {
-    // TODO: Set default type, and change name of function
     static deploySimple(tokenInfo, deployer) {
-        const erc20 = new ERC20()
-        return erc20.deploy(tokenInfo, deployer, ERC20_TYPES.MINIMUM)
+        const erc20 = new ERC20(ERC20_ABI_CODE[ERC20_TYPES.SIMPLE].abi)
+        return erc20.deploy(tokenInfo, deployer, ERC20_TYPES.SIMPLE)
     }
 
-    static deployFull(tokenInfo, deployer) {
-        const erc20 = new ERC20()
+    static deploy(tokenInfo, deployer) {
+        const erc20 = new ERC20(ERC20_ABI_CODE[ERC20_TYPES.FULL].abi)
         return erc20.deploy(tokenInfo, deployer, ERC20_TYPES.FULL)
     }
 
-    constructor(tokenAddress) {
+    static async create(tokenAddress) {
+        const erc20 = new ERC20(ERC20_ABI_CODE[ERC20_TYPES.SIMPLE].abi, tokenAddress)
+
+        const getAccount = new Method({
+            name: 'getAccount',
+            call: 'klay_getAccount',
+            params: 2,
+            inputFormatter: [formatters.inputLogFormatter, formatters.inputDefaultBlockNumberFormatter],
+            requestManager: erc20._requestManager,
+        }).createFunction()
+
+        const result = await getAccount(tokenAddress)
+
+        if (result.accType !== 2) throw new Error(`Invalid token contract address(${tokenAddress})`)
+        if (result.account.codeHash !== CODE_HASH.SIMPLE) {
+            erc20.options.jsonInterface = ERC20_ABI_CODE[ERC20_TYPES.FULL].abi
+            if (result.account.codeHash !== CODE_HASH.FULL) {
+                console.warn(`This token contract is not deployed through caver.klay.ERC20 so it may not work properly.`)
+            }
+        }
+
+        return erc20
+    }
+
+    constructor(abi, tokenAddress) {
         if (tokenAddress && !isAddress(tokenAddress)) {
             throw new Error(`Invalid token contract address: The address of token contract is ${tokenAddress}.`)
         }
-        super(ERC20_ABI_CODE[ERC20_TYPES.FULL].abi, tokenAddress)
+        super(abi, tokenAddress)
     }
 
     clone(tokenAddress) {
-        return new this.constructor(tokenAddress)
+        return new this.constructor(this.options.jsonInterface, tokenAddress)
     }
 
     deploy(tokenInfo, deployer, type) {
@@ -103,58 +133,89 @@ class ERC20 extends Contract {
 
     // Below methods are for ERC20Mintable (extends)
     isMinter(account) {
+        if (this.methods.isMinter === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'isMinter'.`)
+        }
         return this.methods.isMinter(account).call()
     }
 
     mint(msgSender, account, amount) {
+        if (this.methods.mint === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'mint'.`)
+        }
         const gas = 45000
         return this.methods.mint(account, amount).send({ from: msgSender, gas })
     }
 
     addMinter(msgSender, account) {
+        if (this.methods.addMinter === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'addMinter'.`)
+        }
         const gas = 50000
         return this.methods.addMinter(account).send({ from: msgSender, gas })
     }
 
     renounceMinter(minterToRemove) {
-        // TODO: gas check !!! gasUsed is 14000???
+        if (this.methods.renounceMinter === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'renounceMinter'.`)
+        }
         const gas = 30000
         return this.methods.renounceMinter().send({ from: minterToRemove, gas })
     }
 
     // Below methods are for ERC20Burnable (extends)
     burn(msgSender, amount) {
+        if (this.methods.burn === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'burn'.`)
+        }
         const gas = 70000
         return this.methods.burn(amount).send({ from: msgSender, gas })
     }
 
     burnFrom(msgSender, from, amount) {
+        if (this.methods.burnFrom === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'burnFrom'.`)
+        }
         const gas = 70000
         return this.methods.burnFrom(from, amount).send({ from: msgSender, gas })
     }
 
     // Below methods are for ERC20Pausable (extends)
     isPauser(account) {
+        if (this.methods.isPauser === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'isPauser'.`)
+        }
         return this.methods.isPauser(account).call()
     }
 
     addPauser(msgSender, account) {
+        if (this.methods.addPauser === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'addPauser'.`)
+        }
         const gas = 50000
         return this.methods.addPauser(account).send({ from: msgSender, gas })
     }
 
     pause(msgSender) {
+        if (this.methods.pause === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'pause'.`)
+        }
         const gas = 45000
         return this.methods.pause().send({ from: msgSender, gas })
     }
 
     unpause(msgSender) {
+        if (this.methods.unpause === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'unpause'.`)
+        }
         const gas = 30000
         return this.methods.unpause().send({ from: msgSender, gas })
     }
 
     renouncePauser(pauserToRemove) {
-        // TODO: gas check !!! gasUsed is 14000???
+        if (this.methods.renouncePauser === undefined) {
+            throw new Error(`Token contracts deployed with ERC20Simple do not support 'renouncePauser'.`)
+        }
         const gas = 30000
         return this.methods.renouncePauser().send({ from: pauserToRemove, gas })
     }
