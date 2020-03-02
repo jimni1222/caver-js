b364ba8b70515385c8f06373975c92affaf4732c
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2020-03-02 11:19:52 +0900

    Added description of non fungible token

--- packages/caver-klay/caver-klay-kct/kip8.js
+++ packages/caver-klay/caver-klay-kct/kip8.js
@@ -32,6 +32,18 @@ const { toBuffer, isHexStrict, toHex } = require('../../caver-utils/src')
 const { isAddress } = require('../../caver-utils/src')
 
 class KIP8 extends Contract {
+    /**
+     * deploy deploys a KIP-8 token contract to Klaytn network.
+     * `const deployedContract = await caver.klay.KIP8.deploy({
+     *      name: 'TokenName',
+     *      symbol: 'TKN',
+     *  }, '0x{address in hex}')`
+     *
+     * @method deploy
+     * @param {Object} tokenInfo The object that defines the name and symbol of the token to deploy.
+     * @param {String} deployer The address of the account to deploy the KIP-8 token contract.
+     * @return {Object}
+     */
     static deploy(tokenInfo, deployer) {
         validateTokenInfoForDeploy(tokenInfo, KCT_TYPE.NONFUNGIBLE)
 
@@ -67,71 +79,171 @@ class KIP8 extends Contract {
         return isSupported
     }
 
+    /**
+     * name returns the name of the token.
+     *
+     * @method name
+     * @return {String}
+     */
     async name() {
         const name = await this.methods.name().call()
         return name
     }
 
+    /**
+     * symbol returns the symbol of the token.
+     *
+     * @method symbol
+     * @return {String}
+     */
     async symbol() {
         const symbol = await this.methods.symbol().call()
         return symbol
     }
 
+    /**
+     * tokenURI returns the uri of the token.
+     *
+     * @method tokenURI
+     * @param {BigNumber|String|Number} tokenId The id of the token.
+     * @return {String}
+     */
     async tokenURI(tokenId) {
         const tokenURI = await this.methods.tokenURI(formatParamForUint256(tokenId)).call()
         return tokenURI
     }
 
+    /**
+     * totalSupply returns the total amount of tokens stored by the contract.
+     *
+     * @method totalSupply
+     * @return {BigNumber}
+     */
     async totalSupply() {
         const totalSupply = await this.methods.totalSupply().call()
         return new BigNumber(totalSupply)
     }
 
+    /**
+     * tokenOfOwnerByIndex returns the token id at a given index of the tokens list of the requested owner.
+     *
+     * @method tokenOfOwnerByIndex
+     * @param {String} owner The address of the account whose token you want to query.
+     * @param {BigNumber|String|Number} index The index of the token to be searched among the tokens owned by a owner account.
+     * @return {BigNumber}
+     */
     async tokenOfOwnerByIndex(owner, index) {
         const token = await this.methods.tokenOfOwnerByIndex(owner, formatParamForUint256(index)).call()
         return new BigNumber(token)
     }
 
+    /**
+     * tokenByIndex returns the token id at a given index of all the tokens in this contract.
+     *
+     * @method tokenByIndex
+     * @param {BigNumber|String|Number} index The index of the token to query.
+     * @return {BigNumber}
+     */
     async tokenByIndex(index) {
         const token = await this.methods.tokenByIndex(formatParamForUint256(index)).call()
         return new BigNumber(token)
     }
 
+    /**
+     * balanceOf returns the balance of the specified address.
+     * The balance of an account in KIP-8 means that the total number of NFT(Non Fungible Token) owned by the account.
+     *
+     * @method balanceOf
+     * @param {String} account The address of the account whose number of tokens you want to see.
+     * @return {BigNumber}
+     */
     async balanceOf(account) {
         const balance = await this.methods.balanceOf(account).call()
         return new BigNumber(balance)
     }
 
+    /**
+     * ownerOf returns the owner of the specified token id.
+     *
+     * @method ownerOf
+     * @param {BigNumber|String|Number} tokenId The id of the token.
+     * @return {BigNumber}
+     */
     async ownerOf(tokenId) {
         const owner = await this.methods.ownerOf(formatParamForUint256(tokenId)).call()
         return owner
     }
 
+    /**
+     * getApproved returns the approved address for a token id, or zero if no address set.
+     *
+     * @method getApproved
+     * @param {BigNumber|String|Number} tokenId The id of the token.
+     * @return {Boolean}
+     */
     async getApproved(tokenId) {
         const isApproved = await this.methods.getApproved(formatParamForUint256(tokenId)).call()
         return isApproved
     }
 
+    /**
+     * isApprovedForAll returns true if an operator is approved by a given owner.
+     *
+     * @method isApprovedForAll
+     * @param {String} owner The id of the token.
+     * @param {String} operator The id of the token.
+     * @return {Boolean}
+     */
     async isApprovedForAll(owner, operator) {
         const isApprovedForAll = await this.methods.isApprovedForAll(owner, operator).call()
         return isApprovedForAll
     }
 
+    /**
+     * isMinter returns whether the account is minter or not.
+     *
+     * @method isMinter
+     * @param {String} account The address of the account you want to check minter or not.
+     * @return {Boolean}
+     */
     async isMinter(account) {
         const isMinter = await this.methods.isMinter(account).call()
         return isMinter
     }
 
+    /**
+     * paused returns whether or not the token contract's transaction is paused.
+     *
+     * @method paused
+     * @return {Boolean}
+     */
     async paused() {
         const isPaused = await this.methods.paused().call()
         return isPaused
     }
 
+    /**
+     * isPauser returns whether the account is pauser or not.
+     *
+     * @method isPauser
+     * @param {String} account The address of the account you want to check pauser or not.
+     * @return {Boolean}
+     */
     async isPauser(account) {
         const isPauser = await this.methods.isPauser(account).call()
         return isPauser
     }
 
+    /**
+     * approve sets approval with another address to transfer the given token id.
+     *
+     * @method approve
+     * @param {String} to The address of the account to use on behalf of owner for the tokenId.
+     * @param {BigNumber|String|Number} tokenId The id of token the spender allows to use on behalf of the owner.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async approve(to, tokenId, sendParam = {}) {
         const executableObj = this.methods.approve(to, formatParamForUint256(tokenId))
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -139,6 +251,17 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * setApprovalForAll sets or unsets the approval of a given operator.
+     * An operator is allowed to transfer all tokens of the sender on their behalf.
+     *
+     * @method setApprovalForAll
+     * @param {String} to The address of an account to allow/forbid for transfer of all tokens owned by the owner on behalf of the owner.
+     * @param {Boolean} approved Whether to allow sending tokens on behalf of the owner.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async setApprovalForAll(to, approved, sendParam = {}) {
         const executableObj = this.methods.setApprovalForAll(to, approved)
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -146,6 +269,17 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * transferFrom transfers the ownership of a given token id to another address.
+     *
+     * @method transferFrom
+     * @param {String} from The address of the owner or approved of the given token.
+     * @param {String} to The address of the account to receive the token.
+     * @param {BigNumber|String|Number} tokenId The id of token you want to transfer.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async transferFrom(from, to, tokenId, sendParam = {}) {
         const executableObj = this.methods.transferFrom(from, to, formatParamForUint256(tokenId))
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -153,6 +287,20 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * safeTransferFrom safely transfers the ownership of a given token id to another address.
+     * If the target address is a contract, it must implement IERC721Receiver.onERC721Received, which is called upon a safe transfer,
+     * and return the magic value bytes4(keccak256("onERC721Received(address,address,uint256,bytes)")); otherwise, the transfer is reverted.
+     *
+     * @method safeTransferFrom
+     * @param {String} from The address of the owner or approved of the given token.
+     * @param {String} to The address of the account to receive the token.
+     * @param {BigNumber|String|Number} tokenId The id of token you want to transfer.
+     * @param {Buffer|String|Number} data The optional data to send along with the call.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async safeTransferFrom(from, to, tokenId, data, sendParam = {}) {
         if (data && _.isObject(data)) {
             if (data.gas !== undefined || data.from !== undefined) {
@@ -176,6 +324,16 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * addMinter adds an account as a minter that has the permission of MinterRole and can mint.
+     * The account sending transaction to execute the addMinter must be a Minter with a MinterRole.
+     *
+     * @method addMinter
+     * @param {String} account The address of account to add as minter.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async addMinter(account, sendParam = {}) {
         const executableObj = this.methods.addMinter(account)
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -183,6 +341,15 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * renounceMinter renounces privilege of MinterRole.
+     * The account sending transaction to execute the renounceMinter must be a Minter with a MinterRole.
+     *
+     * @method renounceMinter
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async renounceMinter(sendParam = {}) {
         const executableObj = this.methods.renounceMinter()
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -190,6 +357,17 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * mintWithTokenURI creates token with uri and assigns them to account, increasing the total supply.
+     *
+     * @method mintWithTokenURI
+     * @param {String} to The address of the account to which the minted token will be allocated.
+     * @param {BigNumber|String|Number} tokenId The id of token to mint.
+     * @param {String} tokenURI The uri of token to mint.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async mintWithTokenURI(to, tokenId, tokenURI, sendParam = {}) {
         const executableObj = this.methods.mintWithTokenURI(to, formatParamForUint256(tokenId), tokenURI)
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -197,6 +375,15 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * burn destroys a specific KIP-8 token.
+     *
+     * @method burn
+     * @param {BigNumber|String|Number} tokenId The id of token to destroy.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async burn(tokenId, sendParam = {}) {
         const executableObj = this.methods.burn(formatParamForUint256(tokenId))
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -204,6 +391,15 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * pause triggers stopped state that stops sending tokens in emergency situation.
+     * The account sending transaction to execute the pause must be a Pauser with a PauserRole.
+     *
+     * @method pause
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async pause(sendParam = {}) {
         const executableObj = this.methods.pause()
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -211,6 +407,17 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * unpause sets amount as the allowance of spender over the caller’s tokens.
+     * The account sending transaction to execute the unpause must be a Pauser with a PauserRole.
+     *
+     * @method unpause
+     * @param {String} spender The address of the account to use on behalf of owner for the amount to be set in allowance.
+     * @param {BigNumber|String|Number} amount The amount of tokens the spender allows to use on behalf of the owner.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async unpause(sendParam = {}) {
         const executableObj = this.methods.unpause()
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -218,6 +425,16 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * addPauser adds an account as a pauser that has the permission of PauserRole and can pause.
+     * The account sending transaction to execute the addPauser must be a Pauser with a PauserRole.
+     *
+     * @method addPauser
+     * @param {String} account The address of account to add as pauser.
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async addPauser(account, sendParam = {}) {
         const executableObj = this.methods.addPauser(account)
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
@@ -225,6 +442,15 @@ class KIP8 extends Contract {
         return executableObj.send(sendParam)
     }
 
+    /**
+     * renouncePauser renounces privilege of PauserRole.
+     * The account sending transaction to execute the renouncePauser must be a Pauser with a PauserRole.
+     *
+     * @method renouncePauser
+     * @param {Object} sendParam An object with defined parameters for sending a transaction.
+     * @return {Object} A receipt containing the execution result of the transaction for executing the KIP-8 token contract.
+     *                  In this receipt, instead of the logs property, there is an events property parsed by KIP-8 abi.
+     */
     async renouncePauser(sendParam = {}) {
         const executableObj = this.methods.renouncePauser()
         sendParam = await determineSendParams(executableObj, sendParam, this.options.from)
